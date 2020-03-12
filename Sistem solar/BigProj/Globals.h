a3d56c35e8c9b538383a053af511eb5edf1a776e
mariusmihalachi97 <maryuss64@yahoo.com>
2020-03-12 20:37:45 +0200

    hello world

---
+++ Sistem solar/BigProj/Globals.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "glm/glm/glm.hpp"
+using namespace glm;
+class Globals
+{
+public:
+	static const int screenWidth;
+	static const int screenHeight;
+	static const vec3 OX;
+	static const vec3 OY;
+	static const vec3 OZ;
+};
